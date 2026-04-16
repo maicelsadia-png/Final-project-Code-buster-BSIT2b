ab3fff9431407f9f71d3cc72ea5420a863ccfab9
maicelsadia-png <maicelsadia@gmail.com>
2026-04-16 18:34:13 +1300

    Added MongoDB connection using Mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,18 +11,3 @@ const connectDB = async () => {
 };
  
 module.exports = connectDB;
-// In server.js:
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
- 
-const app = express();
-connectDB();
- 
-app.use(cors());
-app.use(express.json());
- 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
